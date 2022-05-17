be0c82dc5f13279bf76e879166155a8d80368cd2
nlom0218 <nlom0218@naver.com>
2022-05-17 22:57:43 +0900

    Overloading

--- src/App.tsx
+++ src/App.tsx
@@ -28,10 +28,30 @@ function App() {
 
   bye(3);
 
-  type Add = (a: number, b: number) => void;
+  // call Signatures
+  type Add = {
+    (a: number, b: number): number;
+    (a: number, b: number, c: number): number;
+  };
 
-  const add: Add = (a, b) => {
-    a + b;
+  type Config = {
+    path: string;
+    state: object;
+  };
+  type Push = {
+    (path: string): void;
+    (config: Config): void;
+  };
+  const push: Push = (config) => {
+    if (typeof config === "string") console.log(config);
+    else {
+      console.log(config.path);
+    }
+  };
+
+  const add: Add = (a, b, c?: number) => {
+    if (typeof b === "string") return a;
+    return a + b;
   };
 
   return null;
