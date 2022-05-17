b1208bb548605614d801700c6884dbcc28799cfc
nlom0218 <nlom0218@naver.com>
2022-05-18 00:12:36 +0900

    Generics Recap, Conclusions

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
   let a: unknown;
@@ -57,16 +57,54 @@ function App() {
 
   type SuperPrint = {
     // Polymorphism
-    <TypePlaceholder>(arr: TypePlaceholder[]): TypePlaceholder;
+    <TypePlaceholder, T>(arr: TypePlaceholder[], b: T): TypePlaceholder;
   };
 
-  const superPrint: SuperPrint = (arr) => arr[0];
+  const superPrint: SuperPrint = (arr, b) => arr[0];
 
-  const e = superPrint([1, 2, 3, 4]);
-  const f = superPrint([true, false, true]);
-  const g = superPrint(["a", "b", "c"]);
-  const h = superPrint([1, 2, true, false, "a"]);
-  console.log(e, f, g, h);
+  const e = superPrint([1, 2, 3, 4], "X");
+  const f = superPrint([true, false, true], 1);
+  const g = superPrint(["a", "b", "c"], true);
+  const h = superPrint([1, 2, true, false, "a"], [1, 2, 3]);
+
+  function basicPrint<V>(a: V[]) {
+    return a[0];
+  }
+
+  const i = basicPrint<number>([1, 2, 3]);
+  const j = basicPrint<boolean>([true, false]);
+
+  type Player<E> = {
+    name: string;
+    extraInfo: E;
+  };
+
+  type NicoExtra = {
+    favFood: string;
+  };
+  type NicoPlayer = Player<NicoExtra>;
+
+  const nico: NicoPlayer = {
+    name: "nico",
+    extraInfo: {
+      favFood: "김치",
+    },
+  };
+
+  const lynn: Player<null> = {
+    name: "lynn",
+    extraInfo: null,
+  };
+
+  type arrNumbers = Array<number>;
+
+  let k: arrNumbers = [1, 2, 3, 4];
+
+  function printAllNumbers(arr: Array<number>) {
+    return arr[0];
+  }
+
+  const [val, setVal] = useState<number>(5);
 
   return null;
 }
