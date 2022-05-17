7a0ee3684c72e6cc170b70cac97050d884d28aaa
nlom0218 <nlom0218@naver.com>
2022-05-17 18:39:18 +0900

    Types of TS part one

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  type Age = number;
+  type Name = string;
+
+  type Player = {
+    name: Name;
+    age?: Age;
+  };
+
+  const playerMaker = (name: string, age?: number): Player => {
+    return {
+      name,
+      age,
+    };
+  };
+
+  const nico = playerMaker("nico", 12);
+
+  console.log(nico.age);
+  return <div className="App"></div>;
 }
 
 export default App;
